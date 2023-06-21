feb2f8ff327cbda07f05fcf1c0bb7c8bb00c8d47
Senon <senon.go@gmail.com>
2023-06-21 14:54:59 +0700

    Nice

---
+++ tes/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  tes
+//
+//  Created by Senon Theodore on 21/06/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
