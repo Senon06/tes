8a7ab885c566dd4b636e18be7b3a2371366b341a
Senon <senon.go@gmail.com>
2023-06-21 14:25:43 +0700

    Initial Commit

---
+++ tes/tesApp.swift
@@ -0,0 +1,17 @@
+//
+//  tesApp.swift
+//  tes
+//
+//  Created by Senon Theodore on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct tesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
